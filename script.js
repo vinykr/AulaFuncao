e95ecaa616dfc726cd4bc9658a01f78f4b05e580
Vinicius Matos <viniciusmatos3333@gmail.com>
2025-04-08 21:42:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const n1 = parsefloat(prompt("Digite o valor de n1"))
+const n2 = parsefloat(prompt("Digite o valor de n2"))
+
+soma(n1,n2)
+
+function soma(n1,n2){
+const resultado = n1 + n2
+console.log("resultado = "+ resultado)
+
+
+} 
